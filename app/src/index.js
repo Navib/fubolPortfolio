ed699f6462d55d5a89ac39bbf54ffd19e4cba527
Ivan Baena <ivan.baena@bedbath.com>
2018-12-11 14:55:54 -0500

    setup react

---
+++ app/src/index.js
@@ -0,0 +1,32 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import promise from 'redux-promise';
+import { store, history } from './store';
+
+import './styles/main.scss';
+import './styles/grid.css';
+
+import App from './components/App';
+import Home from './components/Pages/Home/index';
+
+const Calculator = () => {
+  return <div>Calculator</div>;
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <App>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/calculator" component={Calculator} />
+        </App>
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
