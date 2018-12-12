1842fb545a93b1298fd0ebadb9d5d57d7ffe5961
navib <ivan.baena.dev@gmail.com>
2018-12-12 00:24:19 -0500

    bedtime

--- app/src/components/Pages/Home/Home.js
+++ app/src/components/Pages/Home/Home.js
@@ -5,6 +5,7 @@ import Button from '../../Common/Button';
 import Anchor from '../../Common/Anchor';
 import Jumbotron from '../../Common/Jumbotron';
 import SimpleSlider from '../../Common/Slider';
+import Slide from '../../Common/Slider/Slide';
 
 const Home = () => {
   return (
@@ -25,7 +26,10 @@ const Home = () => {
         speed={500}
         slidesToShow={1}
         slidesToScroll={1}
-      />
+      >
+        <Slide>1</Slide>
+        <Slide>2</Slide>
+      </SimpleSlider>
     </GridContainer>
   );
 };
